877bbd955eeedba1b8cb9b14aba64a7196e2d7b0
Nicolas <niko.d.roger@gmail.com>
2019-07-15 16:53:12 -0400

    config yargs

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar')
 const colors = require('colors/safe');
 
-const argv = require('yargs')
-    .command('listar', 'Imprime en consola la tabla de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
-    })
-    .command('crear', 'Genera un archivo con la tabla de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
-    })
-    .help()
-    .argv;
-
-
+const argv = require('./config/yargs').argv;
 
 let comando = argv._[0];
 
